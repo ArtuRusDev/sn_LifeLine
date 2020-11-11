f923e31126b01e87b9155ce96ee23ba8d337dde4
Artur Ilin <ilinartur2@gmail.com>
2020-11-11 19:31:53 +0800

    Updated parser for "send_friend_request" controller

--- friendsapp/views.py
+++ friendsapp/views.py
@@ -5,14 +5,28 @@ from friendsapp.models import FriendRequests
 
 
 def send_friend_request(request, pk):
-    friend_request = FriendRequests.objects.create(initiator=request.user, target=Person.objects.get(pk=pk))
-    friend_request.save()
+    # Получаю все дупликаты запроса на добавление в друзья
+    try:
+        duplicate = FriendRequests.objects.filter(initiator=Person.objects.get(pk=pk), target=request.user)
+    except Exception:
+        try:
+            duplicate = FriendRequests.objects.filter(initiator=request.user, target=Person.objects.get(pk=pk))
+        except Exception:
+            duplicate = []
+
+    if not duplicate:
+        friend_request = FriendRequests.objects.create(initiator=request.user, target=Person.objects.get(pk=pk))
+        friend_request.save()
+    else:
+        friend_request = duplicate[0]
+        # Изменение статуса на "запрошено" только если статус уже не стоит как "подтвержденный"
+        if friend_request.status != 1:
+            friend_request.status = 0
+            friend_request.save()
+
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 class FriendsList(ListView):
     model = FriendRequests
     template_name = 'friendsapp/friends.html'
-
-    def get_queryset(self):
-        return FriendRequests.objects.filter(status=1)
