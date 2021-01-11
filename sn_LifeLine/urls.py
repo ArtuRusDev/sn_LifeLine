2695bf8f478fde747a9d5f2d22ef766f155b1767
ArtuRusDev <ilinartur2@gmail.com>
2021-01-11 15:12:02 +0800

    Add debug tool

--- sn_LifeLine/urls.py
+++ sn_LifeLine/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 import mainapp
-import messengerapp.views as messengerapp
 from mainapp.views import main_page
 from sn_LifeLine import settings
 
@@ -38,4 +36,6 @@ urlpatterns = [
 handler404 = mainapp.views.handler404
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
