9dfb554277aabe22e082f6a056f57ec3142af6a8
Artur Ilin <ilinartur2@gmail.com>
2022-04-25 19:26:24 +0800

    Profile edit form completely finished

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
-$('body').on('click', '.b-card-item__extra-actions-btn', function (e) {
+$body = $('body');
+$body.on('click', '.b-card-item__extra-actions-btn', function (e) {
     let $actions_block = $(e.currentTarget).parent().find('.b-extra-actions');
     console.log($actions_block);
     $('.b-extra-actions:not(.d-none)').not($actions_block).addClass('d-none');
@@ -6,7 +7,7 @@ $('body').on('click', '.b-card-item__extra-actions-btn', function (e) {
     $actions_block.toggleClass('d-none');
 });
 
-$('body').on('click', '.b-extra-actions__cancel-btn', function (e) {
+$body.on('click', '.b-extra-actions__cancel-btn', function (e) {
     $(e.currentTarget).parent().toggleClass('d-none');
 });
 
@@ -30,7 +31,7 @@ $('.js-card-dropdowns > div').click(function (e) {
     }
 });
 
-$('body').on('click', '.js-follow-community-btn', function (e) {
+$body.on('click', '.js-follow-community-btn', function (e) {
     let $btn = $(e.currentTarget);
     let pk = $btn.attr('data-community-id');
     let $card_wrap = $('.b-card-item[data-community-id="' + pk + '"]').parent();
@@ -45,6 +46,33 @@ $('body').on('click', '.js-follow-community-btn', function (e) {
         }
     });
 });
+
+$('.js-profile-attach-img-btn').on('click', function (e) {
+    e.preventDefault();
+    let $needle_input = $('.b-profile-edit-img').parent().find('input[name="avatar"]');
+    $needle_input.click();
+});
+
+$('input#id_avatar').on('change', function (e) {
+    let $profile_img_wrap = $('.b-profile-edit-img');
+    let file = $(e.currentTarget)[0].files[0];
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        $profile_img_wrap.find('.b-profile-edit-img__img').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(file);
+    $profile_img_wrap.addClass('b-profile-edit-img_filled');
+    $profile_img_wrap.parent().find('input#avatar-clear_id').prop('checked', false);
+});
+
+$('.js-profile-clear-img').on('click', function (e) {
+    let $profile_img_wrap = $('.b-profile-edit-img');
+    $profile_img_wrap.parent().find('input[name="avatar"]').val(null);
+    $profile_img_wrap.parent().find('input#avatar-clear_id').prop('checked', true);
+    $profile_img_wrap.find('.b-profile-edit-img__img').attr('src', '');
+    $profile_img_wrap.removeClass('b-profile-edit-img_filled');
+});
+
 
 function subscribe_community(pk) {
     $.ajax({
